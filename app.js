3452b7b49a2da4bfe1211cda4889c2fff4ad0fb2
kaen <kusdsuna@naver.com>
2020-11-15 01:44:18 +0900

    Add key name

---
+++ app.js
@@ -0,0 +1,9 @@
+// 주요 명칭
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const fill = document.getElementsByClassName('fill');
+const paint = document.getElementsByClassName('paint');
+const save = document.getElementsByClassName('save');
+const return = document.getElementsByClassName('return');
+
+// ctx
