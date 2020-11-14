5527ad8f17ff169c659f1cca46899f821a1a3a15
kaen <kusdsuna@naver.com>
2020-11-15 01:57:29 +0900

    Add ctx 'fillRect', 'fillStyle', 'strokeStyle', 'lineWidth'

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ctx = canvas.getContext('2d');
 const fill = document.getElementsByClassName('fill');
 const paint = document.getElementsByClassName('paint');
 const save = document.getElementsByClassName('save');
-const return = document.getElementsByClassName('return');
 
 // ctx
+ctx.fillRect(0,0,800,800);
+ctx.fillStyle = 'white';
+ctx.strokeStyle = '2c2c2c';
+ctx.lineWidth = 2.5;
